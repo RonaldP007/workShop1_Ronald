3deab1858e9a69fe2526b60fe0a5c0dc3e1629d5
RonaldP007 <paniaguarpg007@gmail.com>
2018-09-18 20:30:43 -0600

    finalizado

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+<head>
+    <meta charset="UTF-8">
+    <title>Hello  world php</title>
+</head>
+<body>
+    <div>
+        <?php echo '<p><h2>Hello world</h2></p>'; ?>
+        <?php echo '<img src="imagen.jpg">'; ?>
+    </div>
+</body>
+</html>
